419099e5bd0c3ecf437740964f651c9204fd77ee
Eleve <ali.talhaoui95@gmail.com>
2021-12-25 16:18:25 +0100

    commande 5 + docu + dossier

---
+++ commande_5.cpp
@@ -0,0 +1,32 @@
+/**
+@file : commande_5.cpp
+Projet : minesweeper
+@author : TALHAOUI Ali 
+@version : 1 22/12/21
+@brief : fonction de recherche d'une case a demasquer
+ */
+#include<iostream>
+#include"commandes.h"
+
+/**
+@brief : trouve une case a demasquer ou affiche une erreur si elle n'en troupe pas
+@return : l'index d'une case
+*/
+int find_move(){
+    assert(game_won);
+    //saisi du nombre de lignes et de colonnes
+    int nbLignes, nbColonnes;
+    cin >> nbLignes >> nbColonnes;
+    
+    //trouve un case non demasquee
+    char cara = ' ';
+    int i = 0;
+    while (cara != '.'){
+        cin >> cara;
+        if (cara == '|')
+            i++;
+    }
+
+    int indexLigne = i/(nbColonnes+1);
+    return indexLigne*nbColonnes + i%(nbColonnes+1) - 1;
+}
