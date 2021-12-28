c0c129ce38b5d3591243911ae2be8fca37a1d528
Eleve <ali.talhaoui95@gmail.com>
2021-12-28 21:15:33 +0100

    pointeur normal

--- grille.cpp
+++ grille.cpp
@@ -10,7 +10,9 @@
 void init_grille(Grille* grille, const int nbLignes, const int nbColonnes){
     grille->nbLignes = nbLignes;
     grille->nbColonnes = nbColonnes;
-    grille = new Case[nbColonnes*nbLignes];
+    grille->cases = new Case*[nbLignes];
+    for (int i = 0 ; i < nbLignes ; i++)
+        grille->cases[i] = new Case[nbColonnes];
 }
 
 /**
@@ -23,42 +25,61 @@ void init_cases(Grille* grille, int indexMines[], int nbMines){
     //initialisation des valeurs par défauts des cases
     for (int i = 0 ; i < grille->nbLignes ; i++){
         for (int j = 0 ; j < grille->nbColonnes ; j++){
-            grille[i*grille->nbColonnes+j].estMine = false;
-            grille[i*grille->nbColonnes+j].estDecouvert = false;
-            grille[i*grille->nbColonnes+j].estMarquee = false;
-            grille[i*grille->nbColonnes+j].nbMinesEnvironnantes = 0;
+            grille->cases[i*grille->nbColonnes+j].estMine = false;
+            grille->cases[i*grille->nbColonnes+j].estDecouvert = false;
+            grille->cases[i*grille->nbColonnes+j].estMarquee = false;
+            grille->cases[i*grille->nbColonnes+j].nbMinesEnvironnantes = 0;
+        }
     }
 
     //initialisation des cases contenant des mines
     for (int i = 0 ; i < nbMines ; i++)
-        grille[indexMines[i]]->estMine = true;
+        grille->cases[indexMines[i]].estMine = true;
 
     //initialisations des cases autour des mines
     for (int i = 0 ; i < grille->nbLignes*grille->nbColonnes ; i++){
-        if (grille[i].estMine){
-            if (i > 0){
-                grille[i-grille->nbColonnes-1].nbMinesEnvironnantes++;
-                grille[i-grille->nbColonnes].nbMinesEnvironnantes++;
-                grille[i-grille->nbColonnes+1].nbMinesEnvironnantes++;
-            }
-            if (i < grille->nbLignes-1){
-                grille[i+grille->nbColonnes-1].nbMinesEnvironnantes++;
-                grille[i+grille->nbColonnes].nbMinesEnvironnantes++;
-                grille[i+grille->nbColonnes+1].nbMinesEnvironnantes++;
-            }
-            if (j > 0)
-                grille[i-1].nbMinesEnvironnantes++;
+        if (grille->cases[i].estMine){
+            if (i >= grille->nbColonnes)
+                grille->cases[i-grille->nbColonnes-1].nbMinesEnvironnantes++;
+                grille->cases[i-grille->nbColonnes].nbMinesEnvironnantes++;
+                grille->cases[i-grille->nbColonnes+1].nbMinesEnvironnantes++;
+            if (i < grille->nbColonnes*(grille->nbLignes-1)-1)
+                grille->cases[i+grille->nbColonnes-1].nbMinesEnvironnantes++;
+                grille->cases[i+grille->nbColonnes].nbMinesEnvironnantes++;
+                grille->cases[i+grille->nbColonnes+1].nbMinesEnvironnantes++;
+            if (i % grille->nbColonnes != 0)
+                grille->cases[i-1].nbMinesEnvironnantes++;
 
-            if (j < grille->nbColonnes-1)
-                grille[i+1].nbMinesEnvironnantes++;
+            if (i % (grille->nbColonnes-1) != 0 && i != 0)
+                grille->cases[i+1].nbMinesEnvironnantes++;
+
+            if (i >= grille->nbColonnes && (i % grille->nbColonnes != 0)
         }
     }
 
 }
 
-
 int main(){
-    Grille* grille;
-    init_grille(grille, 4, 6);
-    init_cases(grille, {1, 5, 4, 6, 7}, 5); 
+    Grille grille;
+    int nbLignes = 4, nbColonnes = 6;
+    int indexMines[] = {1, 5, 12, 7, 19};
+    init_grille(&grille, nbLignes, nbColonnes);
+    /*init_cases(&grille, indexMines, 4);
+    for (int i = 0 ; i < nbLignes ; i++){
+        for(int k = 0 ; k < nbColonnes ; k++)
+            cout << " ___";
+        cout << '\n';
+        for(int j = 0 ; j < nbColonnes ; j++){
+            cout << "| ";
+            if (grille.cases[i*nbColonnes+j].estMine)
+                cout << "M ";
+            else if (grille.cases[i*nbColonnes+j].nbMinesEnvironnantes == 0)
+                cout << "  ";
+            else
+                cout << grille.cases[i*nbColonnes+j].nbMinesEnvironnantes << " ";
+        }
+        cout << "|\n";
+    }
+    for(int k = 0 ; k < nbColonnes ; k++)
+        cout << " ___";*/
 }
