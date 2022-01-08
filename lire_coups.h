226f3e7903a426db233f6290da43e352e913637a
Dhia78 <94971958+Dhia78@users.noreply.github.com>
2022-01-08 21:39:06 +0100

    Update lire_coups.h

--- lire_coups.h
+++ lire_coups.h
@@ -3,7 +3,7 @@
 Projet : minesweeper
 @author : TALHAOUI Ali 
 @version : 1 21/12/21
-@brief : fonction d'interpretation d'un coups entre par l'utilisateur
+@brief : fonction d'interpretation d'un coup entré par l'utilisateur
 */
 #ifndef LIRE_COUPS
 #define LIRE_COUPS
@@ -16,7 +16,7 @@ struct Coups{
 };
 
 /**
-@brief : permet d'interpréter un coups entre par l'utilsateur et d'affecter les valeurs correspondante à une variable de type Coups
+@brief : permet d'interpréter un coup entré par l'utilsateur et d'affecter les valeurs correspondantes à une variable de type Coups
 @param [in, out] coups : la variable de type Coups
 */
 void lireCoups(Coups& coups);
