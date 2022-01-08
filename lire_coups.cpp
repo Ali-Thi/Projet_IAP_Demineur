adc7e38a339defad6bcb3d0db261c704bdfb95b5
Dhia78 <94971958+Dhia78@users.noreply.github.com>
2022-01-08 21:38:30 +0100

    Update lire_coups.cpp

--- lire_coups.cpp
+++ lire_coups.cpp
@@ -3,14 +3,14 @@
 Projet : minesweeper
 @author : TALHAOUI Ali 
 @version : 1 21/12/21
-@brief : fonction d'interpretation d'un coups entre par l'utilisateur
+@brief : fonction d'interpretation d'un coups entré par l'utilisateur
 */
 #include<iostream>
 #include"commandes.h"
 #include"lire_coups.h"
 
 /**
-@brief : permet d'interpréter un coups entre par l'utilsateur et d'affecter les valeurs correspondante à une variable de type Coups
+@brief : permet d'interpréter un coup entré par l'utilsateur et d'affecter les valeurs correspondantes à une variable de type Coups
 @param [in, out] coups : la variable de type Coups
 */
 void lireCoups(Coups& coups){
